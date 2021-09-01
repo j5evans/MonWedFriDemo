fa4a28859dc9a222bad008cdbc27b689676403ca
j5evans <j5evans@student.bridgew.edu>
2021-09-01 15:46:10 -0400

    commit test 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// GitHub test, part 1
+	// GitHub test, part 2
         System.out.println("Hello World");
     }
 }
